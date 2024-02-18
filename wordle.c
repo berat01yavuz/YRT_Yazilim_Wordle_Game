526e9d94561971fdb5b748a9f69bc2cfc1bfd118
Berat Yavuz <beratyavuz340@gmail.com>
2024-02-19 00:37:12 +0300

    tablo yazdırma fonksiyonu tanımladık

--- wordle.c
+++ wordle.c
@@ -41,3 +41,30 @@ işlemi için fonksiyon tanımladık. for döngüsünde strlen komutu ile kelime
 harf sayısı kadar döngü döndürdük. Böylece tahmin edilen kelimedeki i. harfin seçtiğimiz kelimedeki i. harf
 ile kıyaslamasını yaptırdık ve eğer aynı ise yerine yazmasını değilse "_" koymasını sağladık. 
 */
+
+void tabloyazdir() {
+    printf("\n\n\t\t\t WORDLE OYUNU \n\n");
+    for (i = 0; i < indx; i++) {
+        printf("\t\t");
+        for (j = 0; j < strlen(secilenkelime); j++) {
+            if (i == indx - 1) {
+                if (secilenkelime[j] == tahmin[j]) {
+                    printf(" [ ");
+                    printf("%c", tablo[i][j]);
+                    printf(" ] ");
+                } else
+                    printf(" [ %c ]", tablo[i][j]);
+            } else
+                printf(" [ %c ]", tablo[i][j]);
+        }
+        printf("\n\n");
+    }
+}
+
+/*
+Burada bir önceki fonksiyonda kıyasladığımız harfleri oluşturduğumuz tabloya yazdıracak
+bir fonksiyon tanımladık. Tablo matrisindeki ilk dizi olan index sayısı kadar for döngüsünü döndürdük.
+Her karakteri deneyen bir if fonksiyonu tanımladık ve eğer (index - 1) ile uyuşursa değeri ekrana yazdırmasını 
+eğer uyuşmazsa boşluk yazdırmasını sağladık.
+
+*/
