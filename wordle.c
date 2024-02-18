71f8ed96e72caae17c7d2dad5108d85075b4e1e1
Berat Yavuz <beratyavuz340@gmail.com>
2024-02-19 00:23:38 +0300

    kelime seçme fonksiyonu tanımladık

--- wordle.c
+++ wordle.c
@@ -9,3 +9,15 @@
 char kelimeler[kelime_no][kelime_boyut] = {"kalem", "lambacı", "hakan", "perde"};
 char secilenkelime[kelime_boyut], tahmin[kelime_boyut], tablo[6][kelime_boyut], bulunanharfler[kelime_boyut];
 static int indis, indx, sonuc = 0, i, j;
+
+void kelimesecim() {
+    srand(time(0));
+    indis = rand() % kelime_no;
+    strcpy(secilenkelime, kelimeler[indis]);
+    indx = 0;
+}
+/*
+Kelime seçme işlemini yapmak için bir fonksiyon tanımladık. Burada srand, rand komutu ile
+belirlediğimiz kelime aralığında seçim yaptırdık. strcpy komutu ile secilenkelime konumuna
+kelimeler[indis] dizi değerini atadık. 
+*/
